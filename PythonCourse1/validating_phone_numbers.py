96f30d70bd6213693921d71ab95ec81f3592ffda
Carlos Angel <carangpatmar@gmail.com>
2020-09-16 20:56:34 -0500

    Add a new script of 'validating_phone_numbers.py'

--- PythonCourse1/validating_phone_numbers.py
+++ PythonCourse1/validating_phone_numbers.py
@@ -1,1 +1,25 @@
 #!/usr/bin/env python3
+
+import re
+
+for i in range(int(input())):
+    pho_number = input()
+    match = re.search(r'[789]\d{9}$',pho_number)
+    if match:
+        print(True)
+    else:
+        print(False)
+
+'''
+
+IN:
+2
+9587456281
+1252478965
+
+OUT:
+YES
+NO
+
+
+'''
