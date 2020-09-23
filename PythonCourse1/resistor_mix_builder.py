d3d6f79d17971230e9ce8f4cfc10cba3f3c47abc
Carlos Angel <carangpatmar@gmail.com>
2020-09-23 18:00:45 -0500

    Update the check_reisstor_value() function, and add the decision to the main function.
    check_resistor_value() function was with a simple list of lists, and right now it is
    a lsit of dictionaries which males easier to search through the keys.
    I solved the resistor_value_status problem, where it always send False status,
    the problem was the next: I had an else into the for loop, so no matter the if found
    the result, it continues to assign a False that was in the else statement.

--- PythonCourse1/resistor_mix_builder.py
+++ PythonCourse1/resistor_mix_builder.py
@@ -9,21 +9,35 @@ import re
 def ask_value():
     state = True
     while state == True: #Ask to user the value while the input is not in the format
-        resistor_value = input("What value do you want? (noLetter = ohms, K = KiloOhms, M = MegaOmhs): ")
-        if re.search(r"[KM]$",resistor_value): #Check if the input ends with KM
+        resistor_value = input("What value do you want? (H = ohms, K = KiloOhms, M = MegaOmhs): ")
+        if re.search(r"[HKM]$",resistor_value): #Check if the input ends with KM
             state = False
     return resistor_value
 
 def check_resistor_value(resistor_value):
+
+    resistor_value_status = False
+    resistor_value_quantity = 0
+
     with open("resistors_database.csv") as file:
-        csv_f = csv.reader(file)
-        next(csv_f, None) #Skip the header of CSV file
-        for row in csv_f:
-            if resistor_value == row[0]:
+        csv_f = csv.DictReader(file)
+        #next(csv_f, None) #Skip the header of CSV file
+        res_list = [] #make a list of the dictionaries
+        for data in csv_f:
+            res_list.append(dict(data)) 
+        
+        for res_dict in res_list: #go through every dictionary of th e list
+            if res_dict["value"] == resistor_value: #check if the asked value is in the data base
                 resistor_value_status = True
-            else:
-                resistor_value_status = False
-    return resistor_value_status
+                resistor_value_quantity = res_dict["quantity"]
+    
+    return resistor_value_status, resistor_value_quantity
 
 
-check_resistor_value(ask_value())
+
+resistor_value_status, resistor_value_quantity = check_resistor_value(ask_value())
+
+if resistor_value_status and resistor_value_quantity != '0':
+    print("Hey! Friend you have {} reistors of the asked value.".format(resistor_value_quantity))
+else:
+    None
