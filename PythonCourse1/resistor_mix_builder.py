55ccdd17e3e3ae5404b46e7a0050970fdcdaf987
Carlos Angel <carangpatmar@gmail.com>
2020-09-25 23:21:41 -0500

    Add a new function to resistor_mix_builder script, which make a list od the real resistors values from the database

--- PythonCourse1/resistor_mix_builder.py
+++ PythonCourse1/resistor_mix_builder.py
@@ -33,11 +33,34 @@ def check_resistor_value(resistor_value):
     
     return resistor_value_status, resistor_value_quantity
 
+def make_mix_resistors(resistor_value):
+    
+    #read database and make a list of the available resistors
+    with open("resistors_database.csv") as file: #open the csv document
+        csv_f = csv.DictReader(file)
+        database_list = []
+        for row in csv_f: #iterate in the csv's rows, which are dictionaries
+            dict_row = dict(row)
+            if re.search(r"H$",dict_row["value"]):
+                split_list = re.split("H$",dict_row["value"])
+                database_list.append(int(split_list[0]))
+            elif re.search(r"K$",dict_row["value"]):
+                split_list = re.split("K$",dict_row["value"])
+                database_list.append(int(split_list[0])*1000)
+            else:
+                split_list = re.split("M$",dict_row["value"])
+                database_list.append(int(split_list[0])*1000000)
+        print(database_list)
 
+        
 
-resistor_value_status, resistor_value_quantity = check_resistor_value(ask_value())
+    return None
+
+resistor_value = ask_value()
+resistor_value_status, resistor_value_quantity = check_resistor_value(resistor_value)
 
 if resistor_value_status and resistor_value_quantity != '0':
     print("Hey! Friend you have {} reistors of the asked value.".format(resistor_value_quantity))
 else:
-    None
+    print(resistor_value)
+    make_mix_resistors(resistor_value)
